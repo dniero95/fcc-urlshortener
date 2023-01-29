9cd6b0ac9d991f592603cde0132f221d3e476cae
dniero95 <darioniero@icloud.com>
2023-01-29 22:40:07 +0100

    comment url check

--- index.js
+++ index.js
@@ -30,22 +30,22 @@ app.post('/api/shorturl', function (req, res) {
   let regex = /^https?:\/\/(www\.)?[a-zA-Z]+\.[a-zA-Z]{2,}[\/\w-]*$/;
 
   let url = req.body.url.trim();
-  if (regex.test(url)) {
-    let short_url;
-    const keys = cache.keys();
-    if(!keys.length){
-      short_url = 0
-      cache.set(short_url, url, 60 * 60 * 24);
-    }else{
-      let max = Math.max(...keys);
-      short_url = ++max;
-      cache.set(short_url, url, 60 * 60 * 24);
-    }
-    res.json({ original_url: url, short_url: short_url });
-
+  let short_url;
+  const keys = cache.keys();
+  if (!keys.length) {
+    short_url = 0
+    cache.set(short_url, url, 60 * 60 * 24);
   } else {
-    res.json({ error: 'invalid url' });
+    let max = Math.max(...keys);
+    short_url = ++max;
+    cache.set(short_url, url, 60 * 60 * 24);
   }
+  res.json({ original_url: url, short_url: short_url });
+  // if (regex.test(url)) {
+
+  // } else {
+  //   res.json({ error: 'invalid url' });
+  // }
 });
 
 app.get('/api/shorturl/:short_url', (req, res) => {
